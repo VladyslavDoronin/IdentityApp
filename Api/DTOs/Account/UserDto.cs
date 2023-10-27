a390577a86c74cfd7c7ccbad133e71fd6075b8ed
Vladyslav <vladyslav.doronin@gmail.com>
2023-10-27 09:29:37 +0300

    Api project setup, AspNetCore Identity added

---
+++ Api/DTOs/Account/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.DTOs.Account
+{ 
+    /// <summary>
+    /// DTO - Data transfer object
+    /// </summary>
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JWT { get; set; }
+    }
+}
