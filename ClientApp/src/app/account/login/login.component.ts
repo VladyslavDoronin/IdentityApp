b5248db3828d0e63f71e7a40ad58793552475d6e
Vladyslav <vladyslav.doronin@gmail.com>
2023-10-30 16:34:18 +0200

    User persistence in Angular

--- ClientApp/src/app/account/login/login.component.ts
+++ ClientApp/src/app/account/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AccountService } from '../account.service';
+import { take } from 'rxjs';
+import { User } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-login',
@@ -12,11 +14,28 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup = new FormGroup({});
   submitted = false;
   errorMessages: string[] = [];
+  returnUrl: string|null = null;
 
   constructor(private accountService: AccountService,
     private formBuilder: FormBuilder,
-    private router: Router){
-      
+    private router: Router,
+    private activatedRoute: ActivatedRoute){
+      this.accountService.user$.pipe(take(1)).subscribe({
+        next: (user:User | null) => {
+          if(user){
+            this.router.navigateByUrl('/');
+          }
+          else{
+            this.activatedRoute.queryParamMap.subscribe({
+              next:(params: any) => {
+                if(params){
+                  this.returnUrl= params.get('returnUrl');
+                }
+              }
+            })
+          }
+        }
+      })
     }
   
   ngOnInit(): void {
@@ -38,7 +57,12 @@ export class LoginComponent implements OnInit {
     {
       this.accountService.login(this.loginForm.value).subscribe({
         next: (response: any) => {
-          
+          if(this.returnUrl){
+            this.router.navigateByUrl(this.returnUrl);
+          }
+          else{
+            this.router.navigateByUrl('/');
+          }
         },
         error: error => {
           if(error.error.errors){
